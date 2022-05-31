ac9b0fea9140b4210239af214db2c8acf9c2a030
ivansmialko <ivansmyalko@gmail.com>
2022-06-01 00:10:16 +0300

    Новостворений XCode проект

---
+++ BayraktarEngine/Shared/BayraktarEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayraktarEngineApp.swift
+//  Shared
+//
+//  Created by Alexei Liaskoskyi on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BayraktarEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
