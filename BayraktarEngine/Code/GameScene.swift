4ad6abfb68fb3266645a41d002069c31abecd417
ivansmialko <ivansmyalko@gmail.com>
2022-06-01 04:35:42 +0300

    Застосування двигуна Гілка створена для тестових проектів та прикладів, за якими можна використовувати двигун В проекті: - Декілька 3D об`єктів - Камера першого лиця - Управління рухом камери за допомогою клавіатури

--- BayraktarEngine/Code/GameScene.swift
+++ BayraktarEngine/Code/GameScene.swift
@@ -2,7 +2,33 @@ import MetalKit
 
 struct GameScene
 {
-    lazy var models: [Model] = []
+    lazy var house: Model = {
+        Model(name: "lowpoly-house.obj")
+    }()
+    
+    lazy var ground: Model = {
+        var ground = Model(name: "plane.obj")
+        ground.tiling = 16
+        ground.scale = 40
+        return ground
+    }()
+    
+    lazy var obamium: Model =
+    {
+        var obamium = Model(name: "obamium.obj")
+        obamium.position = [0, 0, 4]
+        obamium.rotation = [0, Float(45).degreesToRadians, 0]
+        return obamium
+    }()
+    
+    lazy var btr: Model =
+    {
+        var car = Model(name: "Ukrainian_Z_BTR.obj")
+        car.position = [3, -0.1, 8]
+        return car
+    }()
+    
+    lazy var models: [Model] = [ground, house, obamium, btr]
     
     var camera = FirstPersonCamera()
     
